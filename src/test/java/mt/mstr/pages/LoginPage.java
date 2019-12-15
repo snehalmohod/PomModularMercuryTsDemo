df5741d41acd1cd6b5306843340fbfd0d7402856
snehalmohod <snehalmohod93@gmail.com>
2019-12-15 16:55:21 +0530

    updated login page 1.1

--- src/test/java/mt/mstr/pages/LoginPage.java
+++ src/test/java/mt/mstr/pages/LoginPage.java
@@ -26,6 +26,8 @@ public class LoginPage {
 	{
 		System.out.println("In enterUsername page to");
 		objusername.sendKeys(username);
+		System.out.println("hello");
+		System.out.println("hi");
 	}
 	public void enterPassword(String password)
 	{
